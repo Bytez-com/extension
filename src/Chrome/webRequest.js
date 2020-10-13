463f884a40369b8a1cd224ac6859fcaed645c5b6
Nawar Alsafar <NawarA@users.noreply.github.com>
2020-10-13 16:07:48 -0700

    a

--- src/Chrome/webRequest.js
+++ src/Chrome/webRequest.js
@@ -1,9 +1,11 @@
 /* global chrome */
-chrome.webRequest.onBeforeRequest.addListener(
-  function (details) {
-    console.log(details);
-    return { cancel: true };
-  },
-  { urls: ["*://*arxiv.org/pdf/*"] },
-  ["blocking"]
-);
+if (chrome && chrome.webRequest) {
+  chrome.webRequest.onBeforeRequest.addListener(
+    function (details) {
+      console.log(details);
+      return { cancel: true };
+    },
+    { urls: ["*://*arxiv.org/pdf/*"] },
+    ["blocking"]
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
 //
 import App from "./App";
+import "./Chrome/webRequest";
 //
 ReactDOM.render(
   <React.StrictMode>
