f1c30dc234beff4718ffaee3c1b13c9f4cfe3a2d
Nawar Alsafar <nawar@gatech.edu>
2021-03-09 14:18:13 -0800

    breaking changes; go to version 2.0.0

---
+++ generateRules.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const fetch = require("node-fetch");
+const {
+  promises: { writeFile }
+} = require("fs");
+
+fetch("https://api.bytez.com/papers")
+  .then(res => res.json())
+  .then(list => console.log(list.length) || list)
+  .then(list =>
+    writeFile(
+      `${__dirname}/public/rules.json`,
+      JSON.stringify(
+        list.map((paperID, index) => ({
+          id: index + 1,
+          priority: 2147483646,
+          action: {
+            type: "redirect",
+            redirect: {
+              url: `https://bytez.com/read/arxiv/${paperID}`
+            }
+          },
+          condition: {
+            isUrlFilterCaseSensitive: false,
+            urlFilter: `*://arxiv.org/pdf/${paperID}`,
+            resourceTypes: ["main_frame"]
+          }
+        })),
+        undefined,
+        2
+      )
+    )
+  );
--- public/background.js
+++ public/background.js
@@ -1,106 +1,118 @@
-const extractPaperID = / /.exec.bind(/(?!pdf\/)(\d+(?:\.\d+)?)[^v.]/);
-let extensionEnabled = true;
-let alreadySyncing = false;
-let whiteList = {};
-let timeoutID;
+/* eslint-disable max-len */
+// Extension event listeners are a little different from the patterns
+// you may have seen in DOM or Node.js APIs. The below event listener
+//  registration can be broken into 4 distinct parts:
 //
+// * chrome      - the global namespace for Chrome's extension APIs
+// * runtime     – the namespace of the specific API we want to use
+// * onInstalled - the event we want to subscribe to
+// * addListener - what we want to do with this event
 //
-// redirect all arix pdf papers to the smart leader
-chrome.webRequest.onBeforeRequest.addListener(
-  ({ url }) => {
-    const whiteListedPaperID = checkIfWhiteListed(url);
+// See https://developer.chrome.com/docs/extensions/reference/events/
+//
+// every 8 hours, update paper white list
+// and add a 1-60 min extra wait time, so tabs dont updates dont collide
+chrome.alarms.create({ periodInMinutes: 60 * 8 });
+chrome.alarms.onAlarm.addListener(updateWhiteList);
+// helpers to sync cache across tabs
+// const storageGet = key =>
+//   new Promise(resolve => chrome.storage.sync.get([key], resolve));
+// const storageSet = obj =>
+//   new Promise(resolve => chrome.storage.sync.get(obj, resolve));
 
-    if (whiteListedPaperID) {
-      chrome.tabs.create({
-        url: `https://bytez.com/read/arxiv/${whiteListedPaperID}`
-      });
-      // eslint-disable-next-line no-script-url
-      return { redirectUrl: "javascript:void(0)" };
-    }
-  },
-  { urls: ["*://arxiv.org/pdf/*"] },
-  ["blocking"]
-);
-// close all tabs that open that (in case anchor had target="none")
-chrome.webNavigation.onCreatedNavigationTarget.addListener(
-  ({ url, tabId }) => {
-    if (checkIfWhiteListed(url)) {
-      chrome.tabs.remove(tabId);
-    }
-  },
-  { url: [{ hostContains: "arxiv.org", pathPrefix: "/pdf" }] }
-);
-// check if extension is enabled; if so, enable or disable sync
-chrome.storage.onChanged.addListener(async ({ settings_v1 }) => {
+// chrome.storage.sync.clear(console.log);
+// allow only one "getWhiteList" request to occur a single re-sync in 8 hours
+async function updateWhiteList() {
   try {
-    extensionEnabled = settings_v1?.newValue?.enabled ?? true;
+    // redownload the list of paper IDs that are white listed
+    const [listOfPaperIDs, whiteList] = await Promise.all([
+      fetch("https://api.bytez.com/papers").then(res => res.json()),
+      fetch(chrome.runtime.getURL("rules.json"))
+        .then(res => res.json())
+        .then(
+          json =>
+            new Map(
+              json.map(({ action: { redirect: { url } } }) => [
+                url.slice(29),
+                true
+              ])
+            )
+        )
+    ]);
 
-    if (extensionEnabled) {
-      await syncSupportedPaperIDs();
-    } else {
-      clearTimeout(timeoutID);
-    }
-  } catch (error) {
-    console.error(error);
-  }
-});
-// is the arxiv paper ID white listed to be opened
-function checkIfWhiteListed(url) {
-  if (extensionEnabled) {
-    const [paperID] = extractPaperID(url);
+    const newPaperIDsToWhiteList = listOfPaperIDs.filter(
+      paperID => whiteList.has(paperID) === false
+    );
+    console.log(
+      "Found",
+      newPaperIDsToWhiteList.length,
+      "new papers to white list",
+      newPaperIDsToWhiteList
+    );
+    // and if it downloads correctly
+    if (newPaperIDsToWhiteList.length) {
+      const batches = [];
 
-    if (whiteList[paperID]) {
-      return paperID;
-    }
-  }
-}
-// only allow one sync to be active
-async function syncSupportedPaperIDs(retry = true) {
-  try {
-    if (alreadySyncing === false) {
-      alreadySyncing = true;
+      for (let batch, i = 0, j = newPaperIDsToWhiteList.length; i < j; ) {
+        batch = [];
 
-      const response = await fetch("https://api.bytez.com/papers");
-      const listOfPaperIDs = await response.json();
+        while (batch.length !== 8 && i < j) {
+          batch.push(newPaperIDsToWhiteList[i++].replace(".", "\\."));
+        }
 
-      if (listOfPaperIDs?.length) {
-        setWhiteList(listOfPaperIDs);
-      } else if (retry) {
-        syncSupportedPaperIDs(false);
+        batches.push(batch.join("|"));
       }
+
+      await Promise.all(batches.map(updateWhiteListRule));
     }
   } catch (error) {
     console.error(error);
-  } finally {
-    alreadySyncing = false;
-    // enable a single re-sync in 8 hours
-    clearTimeout(timeoutID);
-    timeoutID = setTimeout(syncSupportedPaperIDs, 2.88e7);
   }
 }
-function setWhiteList(listOfPaperIDs = []) {
-  // reset the supported object
-  whiteList = {};
-  // set each paperID to true
-  for (const paperID of listOfPaperIDs) {
-    whiteList[paperID] = true;
+const updateWhiteListRule = (paperIDs, index) =>
+  new Promise(resolve =>
+    chrome.declarativeNetRequest.updateSessionRules(
+      { removeRuleIds: [index + 1] },
+      () =>
+        chrome.declarativeNetRequest.updateSessionRules(
+          {
+            addRules: [
+              {
+                id: index + 1,
+                priority: 2147483646,
+                action: {
+                  type: "redirect",
+                  redirect: {
+                    regexSubstitution: "bytez.com/read/arxiv/\\1"
+                  }
+                },
+                condition: {
+                  // eslint-disable-next-line no-useless-escape
+                  regexFilter: `arxiv\\.org\/pdf\/(${paperIDs})`,
+                  resourceTypes: ["main_frame"]
+                }
+              }
+            ]
+          },
+          resolve
+        )
+    )
+  );
+
+updateWhiteList();
+//
+//
+// allow bytez.com to know if the extension is installed
+chrome.runtime.onMessageExternal.addListener(
+  (message, sender, sendResponse) => {
+    sendResponse(true);
   }
-  // cache the list of paper IDs
-  localStorage.listOfPaperIDs = JSON.stringify(listOfPaperIDs);
-}
-// download the supported paper IDs and resync every 8 hours
-syncSupportedPaperIDs();
-// check if this is the first time the user has installed the extension
-chrome.storage?.sync.get("onboarded", ({ onboarded }) => {
-  if (onboarded === undefined) {
+);
+//
+//
+// if this is a new user, ask them to register
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+  if (reason === "install") {
     chrome.tabs.create({ url: "https://bytez.com/welcome" });
-    chrome.storage?.sync.set({ onboarded: true });
   }
 });
-chrome.runtime.onMessageExternal.addListener((request, sender, sendResponse) =>
-  sendResponse(true)
-);
-// reload whitelist from cache
-setWhiteList(
-  localStorage.listOfPaperIDs && JSON.parse(localStorage.listOfPaperIDs)
-);
